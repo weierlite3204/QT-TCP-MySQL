0153edf105bdd2abbaec4f0c9f7ff7d39d1a898c
罗家嵩 <3204866060@qq.com>
2025-11-08 10:26:37 +0800

    1

--- widget.h
+++ widget.h
@@ -31,24 +31,25 @@ private:
     unsigned int port=1210;//端口号
     MyTcpServer *msgserver=NULL;//tcp服务
     debugging *deb=NULL;//调试窗口
-    
+    bool water = false;//浇水
+
     // 图表相关成员变量
     QCustomPlot *customPlot1; // 第一个图表（空气温度、湿度、氧气）
     QCustomPlot *customPlot2; // 第二个图表（土壤温度、湿度、光照）
-    
+
     // 数据存储
     QVector<double> timeData1; // 第一个图表的时间数据
     QVector<double> tempData;  // 空气温度数据
     QVector<double> humidityData; // 空气湿度数据
     QVector<double> oxygenData; // 氧气浓度数据
-    
+
     QVector<double> timeData2; // 第二个图表的时间数据
     QVector<double> soilTempData; // 土壤温度数据
     QVector<double> soilHumidityData; // 土壤湿度数据
     QVector<double> lightData; // 光照强度数据
-    
+
     const int maxDataPoints = 20; // 最大数据点数量
-    
+
     // 初始化图表函数
     void initCharts();
     //界面初始化函数
@@ -58,5 +59,8 @@ private slots:
     void do_msgnewConnection(qintptr socket);//有客户端连接到消息服务器
     void showdata(SensorData);//把接收到的数据在ui界面中展示出来
     void portchange();//用户修改主机端口
+    void on_lightbtn_clicked();//开关灯按钮点击事件
+    void on_waterbtn_clicked();//浇水按钮点击事件
+    void on_exitbtn_clicked();//系统关闭按钮点击事件
 };
 #endif // WIDGET_H
